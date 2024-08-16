4b8ebc22d151dbdd388ce72db903686b59abe22d
VipinDevelops <VipinChaudhary1809@gmail.com>
2024-08-16 12:48:05 +0530

    feat: qs dynamic values

--- src/commands/QsCommand.ts
+++ src/commands/QsCommand.ts
@@ -13,9 +13,11 @@ import {
 	IPersistence,
 } from '@rocket.chat/apps-engine/definition/accessors';
 import { ReplyStorage } from '../storage/ReplyStorage';
-import { sendMessage } from '../helper/message';
+import { replacePlaceholders, sendMessage } from '../helper/message';
 import { t } from '../lib/Translation/translation';
 import { getUserPreferredLanguage } from '../helper/userPreference';
+import { IMessageRaw } from '@rocket.chat/apps-engine/definition/messages';
+import { GetMessagesSortableFields } from '@rocket.chat/apps-engine/server/bridges/RoomBridge';
 
 export class QsCommand implements ISlashCommand {
 	constructor(private readonly app: QuickRepliesApp) {}
@@ -118,8 +120,100 @@ export class QsCommand implements ISlashCommand {
 			read.getPersistenceReader(),
 		);
 		const reply = await replyStorage.getReplyById(user, item.id);
-		if (reply) {
-			await sendMessage(modify, user, context.getRoom(), reply.body);
+
+		// check if room is direct
+
+		const room = context.getRoom();
+		console.log(room);
+		if (room.userIds) {
+			if (room.userIds.length == 2 && !room.creator) {
+				console.log('direct room');
+				// console.log('our id ', user.id);
+				const receiverId = room.userIds.find((id) => id !== user.id);
+				if (receiverId) {
+					// console.log(receiverId);
+					const receiverUser = await read
+						.getUserReader()
+						.getById(receiverId);
+					// console.log(receiverUser);
+					// console.log('after promsie value ');
+					const name = receiverUser.name;
+					const username = receiverUser.username;
+					const email = receiverUser?.emails?.[0]?.address;
+					// console.log(email, 'email');
+					const replacements = {
+						...(name && { name }),
+						...(username && { username }),
+						...(email && { email }),
+					};
+
+					if (reply) {
+						const message = replacePlaceholders(
+							reply?.body.trim(),
+							replacements,
+						);
+						await sendMessage(
+							modify,
+							user,
+							context.getRoom(),
+							message,
+						);
+					}
+				}
+			}
+		} else {
+			// we are in channel with more than 2 user and it have a creator
+			//get prev messgae
+			// find first message that is not ours
+			// console.log('you are in a room');
+			const prevmessages: IMessageRaw[] = await read
+				.getRoomReader()
+				.getMessages(room.id, {});
+
+			const sorted = prevmessages.sort((a, b) => {
+				return (
+					new Date(b.createdAt).getTime() -
+					new Date(a.createdAt).getTime()
+				);
+			});
+
+			// sorted.forEach((val) => {
+			// 	console.log(val.sender.username, '---sorted');
+			// });
+
+			prevmessages.forEach((val) => {
+				console.log(val.sender.username, '---message');
+			});
+			let differnetId = '';
+			for (let i = 0; i < prevmessages.length; i++) {
+				if (prevmessages[i].sender._id !== user.id) {
+					differnetId = prevmessages[i].sender._id;
+				}
+			}
+
+			const receiverUser = await read
+				.getUserReader()
+				.getById(differnetId);
+
+			const name = receiverUser.name;
+			const username = receiverUser.username;
+			const email = receiverUser?.emails?.[0]?.address;
+			const roomName = room.slugifiedName;
+			// console.log(email, 'email');
+			const replacements = {
+				...(name && { name }),
+				...(username && { username }),
+				...(email && { email }),
+				...(roomName && { room: roomName }),
+			};
+
+			if (reply) {
+				const message = replacePlaceholders(
+					reply?.body.trim(),
+					replacements,
+				);
+				await sendMessage(modify, user, context.getRoom(), message);
+			}
 		}
 	}
 }
